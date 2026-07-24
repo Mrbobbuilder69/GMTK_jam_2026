5d2330193dbc460e99f02e473b24c5a1aa7145d2
Mrbobbuilder69 <116387248+Mrbobbuilder69@users.noreply.github.com>
2026-07-24 22:41:47 +0200

    Basic brawler/melee AI
    Full AI detection logic, basic brawler AI that runs at the player when detected
    Runs off scene unique nodes for the player, player is a rigidbody reference for now(will need to be changed)
    Brawler class derives from enemy class, other basic enemy ai types can be implemented from extending the enemyai class
    Haven't implemented damage(to or from the player) or death yet, functions are there to do so

--- Scripts/EnemyAI/Brawler_AI.gd
+++ Scripts/EnemyAI/Brawler_AI.gd
@@ -1,60 +1,66 @@
 class_name BrawlerAI extends EnemyAI
 
-#Raycast parameters:
-#{
-#   position: Vector2 # point in world space for collision
-#   normal: Vector2 # normal in world space for collision
-#   collider: Object # Object collided or null (if unassociated)
-#   collider_id: ObjectID # Object it collided against
-#   rid: RID # RID it collided against
-#   shape: int # shape index of collider
-#  metadata: Variant() # metadata of collider
-#}
 
 @export var rayScanDistance := float(20)
-@export var moveSpeed := float(20)
-@export var rotationVelocity := float(2)
-var tween : Tween
+@export var patrolSpeed := float(80)
+@export var detectedSpeed := float(140)
+@export var patrolRotationVelocity := float(2)
+@export var detectedRotationVelocity := float(4)
+@export var meleeDistance := float(30)
 var canBounce := true
 var newvel:Vector2
 func _ready() -> void:
-	tween = create_tween()
-	rb.linear_velocity = (Vector2.from_angle(rb.rotation))*moveSpeed
+	rb.linear_velocity = (Vector2.from_angle(rb.rotation))*patrolSpeed
+	super._ready()
 
-##Bounces around a closed room
-func onPatrol(delta:float) ->void:
+##Bounces around a closed room, might pull this up to the enemy controller
+func patrolBehaviour(delta:float) ->void:
 	if ray2D == null:
 		print("Ray is null ")
 		return
-    #
+	#
 	ray2D.target_position = Vector2(1,0)*rayScanDistance
 
 	if ray2D.is_colliding() and canBounce:
 
-		rb.linear_velocity = Vector2(0,0)
-	    var velocity := rb.linear_velocity
-		var currentAngle := rb.rotation
-        #get raycast hit information 
+		var velocity := rb.linear_velocity
+		#get raycast hit information 
 		var collisionData := RaycastHitData.new(ray2D.get_collider(),
 		ray2D.get_collision_normal(),ray2D.get_collision_point())
-        #compute bounce, bounce final velocity is inverting the normal component
+		#compute bounce, bounce final velocity is inverting the normal component
 		var normalComponent := velocity.dot(collisionData.normal)*collisionData.normal
 		var tangentialComponent := velocity - normalComponent
 		var bounceVelocity := tangentialComponent - normalComponent
-		newvel = bounceVelocity.normalized()*moveSpeed
-        #tween the rotation to the new bounce position so it doesn't look like a screensaver
-		tween = create_tween()
-		var diff := angle_difference(currentAngle,newvel.angle()) 
-		var targetAngle = currentAngle+diff
-		
-		tween.tween_property(rb,"rotation",targetAngle,abs(diff)/rotationVelocity)
+		newvel = bounceVelocity.normalized()* patrolSpeed * speedModifier
+		#tween the rotation to the new bounce position so it doesn't look like a screensaver
+		rb.linear_velocity = Vector2(0,0)
+		tweenToAngle(newvel.angle(),patrolRotationVelocity)
 		canBounce = false
 	#wait until the tween is done before setting new velocity and position
 	elif not ray2D.is_colliding() and not tween == null:
 		if not tween.is_running():
 			canBounce = true
 			rb.linear_velocity = newvel
-		
 
-func onPlayerDetected() ->void:
-	print("player detected")
+func onDetected() -> void:
+	tween.kill()
+	print("player just detected")
+
+##Runs at the player
+func detectedBehaviour(delta:float) ->void:
+	print("detected")
+	#check if the enemy is in melee range, otherwise rotate towards and run at the player
+	var distanceTo = (rb.position - playerref.position).length()
+	var targetAngle = get_angle_to(playerref.position) + rb.rotation
+	rb.rotation = rotate_toward(rb.rotation, targetAngle, detectedRotationVelocity * delta)
+	if distanceTo > meleeDistance:
+		rb.linear_velocity = Vector2.from_angle(rb.rotation) * detectedSpeed * speedModifier
+	else:
+		rb.linear_velocity = Vector2(0,0)
+		#Insert melee attack and or damage logic here
+
+func onUnDetect() -> void:
+	tween.kill()
+	newvel = Vector2.from_angle(rb.rotation)*patrolSpeed * speedModifier
+	rb.linear_velocity = newvel
+	canBounce = true
--- Scripts/EnemyAI/EnemyAIController.gd
+++ Scripts/EnemyAI/EnemyAIController.gd
@@ -1,11 +1,6 @@
 @abstract class_name EnemyAI extends RigidBody2D
+##classes and enums
 enum AIState {patrol,detected}
-
-var playerref : RigidBody2D
-@export var rb :RigidBody2D
-@export var blood := float(100) #maybe set this up as a percentage
-var currentState := AIState.patrol
-
 class RaycastHitData:
     var collider
     var normal:Vector2
@@ -15,17 +10,95 @@ class RaycastHitData:
         normal = inormal
         point = ipoint
 
+##Editor vars
+@export var rb :RigidBody2D
+@export var maxBlood := float(100) #maybe set this up as a percentage
 @export var ray2D :RayCast2D
-@abstract func onPlayerDetected() ->void
+@export var detectionDistance := float(100)
+##degrees
+@export var fov := float(120)
 
-@abstract func onPatrol(delta:float) ->void
+##Private vars
+var playerref : RigidBody2D
+var currentState := AIState.patrol
+var blood : float
+var speedModifier = 1
+var tween : Tween
+var dead = false
+
+##Functions
+@abstract func detectedBehaviour(delta:float) ->void
+@abstract func patrolBehaviour(delta:float) ->void
+@abstract func onDetected() -> void
+@abstract func onUnDetect() -> void
+
+func _ready() -> void:
+    blood = maxBlood
+    setState(AIState.patrol)
+    playerref = get_node("%Player")
+
+func tweenToAngle(target:float, angVel:float) ->void:
+        tween = create_tween()
+        var currentAngle = rb.rotation
+        var diff := angle_difference(currentAngle,target) 
+        var targetAngle = currentAngle+diff
+        tween.tween_property(rb,"rotation",targetAngle,abs(diff)/angVel)
+
+func onDeath():
+    print("summon corpse and blood stuff here and do other death things")
+
+func onDamage(damangeAmount:float):
+    print("particles and other damage effect stuff")
+    blood -= damangeAmount
+
+#Returns if the player is visible or not, to check state change
+func checkPlayerVisible(playerPosition :Vector2) ->bool:
+    var displacementVector = playerPosition-rb.position
+    var distance = displacementVector.length()
+    if distance > detectionDistance:
+        return false
+    
+    var forwardVector = Vector2.from_angle(rb.rotation)
+    var dp = (forwardVector).dot(displacementVector.normalized())
+    if dp <= cos(deg_to_rad(fov)/2):
+        return false
+    
+
+    var space_state = get_world_2d().direct_space_state
+    var query = PhysicsRayQueryParameters2D.create(rb.position,playerPosition)
+    #BIT MASK, needs to be set via the bitwise operations(check the docks)
+    #change this to whatever the terrain is on
+    query.collision_mask = 1
+    var result = space_state.intersect_ray(query)
+    if result:
+        return false
+    #Call the on detected function
+        
+    return true
+    
 
 func setState(state:AIState) ->void:
     currentState = state
 
 func _physics_process(delta: float) -> void:
+    if blood <= 0 and !dead:
+        onDeath()
+        dead = true
+    if dead:
+        return
     match currentState:
         AIState.patrol:
-            onPatrol(delta)
+            patrolBehaviour(delta)
         AIState.detected:
-            onPlayerDetected()
+            detectedBehaviour(delta)
+    if playerref != null:
+        var playervisible := checkPlayerVisible(playerref.position)
+        if playervisible and currentState != AIState.detected:
+            print("player detected")
+            onDetected()
+            setState(AIState.detected)
+        elif !playervisible and currentState == AIState.detected:
+            onUnDetect()
+            setState(AIState.patrol)
+    else:
+        print("Enemy AI player reference not set")

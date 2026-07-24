17813d0975e48553e2819a56ab561e34fa8eba9e
hatashgames <hatema.main@gmail.com>
2026-07-24 19:35:55 +0100

    Sprinting

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
-@export var speed : float = 20000.0
-@export var footstep_interval : float = 20.0
+@export var speed : float = 100.0
+@export var sprint_speed : float = 150.0
+@export var footstep_interval : float = 30.0
 
 var footstep_distance_counter : float = 0.0
 var prev_position : Vector2
@@ -9,12 +10,20 @@ var prev_position : Vector2
 func _physics_process(delta: float) -> void:
 	var inputVec := Input.get_vector("Left","Right","Up","Down")
 	var mousePos = get_global_mouse_position()
-	linear_velocity = speed*inputVec
+	
+	var cur_speed : float
+	if Input.is_action_pressed("Sprint"):
+		cur_speed = sprint_speed
+	else:
+		cur_speed = speed
+	linear_velocity = cur_speed*inputVec
 	look_at(mousePos)
 	
+	
+	
 	footstep_distance_counter += global_position.distance_to(prev_position)
-	if footstep_distance_counter > footstep_interval and inputVec != Vector2.ZERO:
-		Effects.spawn_sfx("res://Sound/footstep.wav", self, 0.0, 1.0, self.global_position)
+	if footstep_distance_counter > footstep_interval * (cur_speed/speed) and inputVec != Vector2.ZERO:
+		Effects.spawn_sfx("res://Sound/footstep.wav", self, 0.0, cur_speed/speed, self.global_position)
 		Effects.spawn_particles("res://ParticleEffects/footstep_particles.tscn", self.global_position, self, true)
 		footstep_distance_counter = 0.0
 	
